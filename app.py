360547558a9328014fe88f1fe79ab663bb3978f4
Steven Bucholtz <steven@bucholtz@gmail.com>
2018-11-21 05:23:41 +0000

    Added database support for login

--- app.py
+++ app.py
@@ -16,11 +16,23 @@ def verifyuser():
 
 @app.route('/authenticate_user', methods=['POST'])
 def authenticate_user():
-  if request.form['username'] == 'sbucholtz' and request.form['password'] == 'password':
-    return "You have logged in. Welcome Steven."
+  password = request.form['password']
+  username = request.form['username']
+
+  user_exists = query_db('SELECT COUNT(*) FROM USERS WHERE username = ?', [username])
+
+  if(user_exists[0][0] == 1):
+    password_true = query_db('SELECT COUNT(*) FROM USERS WHERE username = ? AND password = ?', (username, password))
+    if(password_true[0][0] == 1):
+      session['login'] = "username"
+      return "You have logged in. Welcome Steven."
+    else:
+      flash('Invalid login. This attempt has been logged.')
+      return redirect(url_for('verifyuser'))
   else:
-    flash('Invalid login. This attempt has been logged.')
+    flash('Login failed.')
     return redirect(url_for('verifyuser'))
+
 
 
 
@@ -31,6 +43,38 @@ def submit():
 @app.errorhandler(418)
 def foureighteen(e):
   return render_template('418.html'), 418
+
+#-----------------------------------------------------------------
+# connection_to_sqlite_database as the name implies handles our
+# connection to the SQLITE database.db database file. We keep
+# the connection open until our database commits/reads have 
+# fully completed. 
+# -----------------------------------------------------------------
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+  
+def isLoggedIn():
+  if(session.get('login') is None):
+    return False
+  else:
+    return True
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
 if __name__ == '__main__':
